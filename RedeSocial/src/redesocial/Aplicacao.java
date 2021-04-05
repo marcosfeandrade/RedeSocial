8714db85d33ed7b802435d5af3de5a649c8e1dba
Irty  Nobre <irtysantos@gmail.com>
2021-04-04 22:51:34 -0300

    Implementar login no main

--- RedeSocial/src/redesocial/Aplicacao.java
+++ RedeSocial/src/redesocial/Aplicacao.java
@@ -3,19 +3,35 @@ package redesocial;
 
 import java.util.Scanner;
 import redesocial.controllers.GerenciamentoConta;
-import redesocial.views.CadastroContaView;
+import redesocial.models.Conta;
 
 public class Aplicacao {
     public static void main(String[] args) {
-        /*GerenciamentoConta gerenciamentoConta = new GerenciamentoConta();
-        Scanner in = new Scanner(System.in);
-        System.out.println("Bem vindo!");
-        System.out.println("1- logar");
-        System.out.println("2-cadastrar?");
-        int resp = in.nextInt();
-        if(resp == 1){
-           CadastroContaView ccv = new CadastroContaView(gerenciamentoConta);
-        } MANTER*/
+        GerenciamentoConta gerenciamentoConta = new GerenciamentoConta();
+        boolean sair = false;
+        Conta conta = null;
+
+        do {
+            System.out.println("Bem vindo!");
+            System.out.println("1- Cadastrar");
+            System.out.println("2- Login");
+            System.out.println("3- Sair");
+
+            Scanner in = new Scanner(System.in);
+
+            switch(in.nextInt()) {
+                case 1:
+                    break;
+                case 2:
+                    String login = in.next();
+                    String senha = in.next();
+                    conta = gerenciamentoConta.logar(login, senha);
+                    break;
+                case 3:
+                    sair = true;
+                    break;
+            }
+        } while (!sair);
     }
     
 }
