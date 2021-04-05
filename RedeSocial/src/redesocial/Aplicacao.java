54c7bc6faa0b196caac65e40bd5bd956adf73cfb
Irty Nobre <irtysantos@gmail.com>
2021-04-04 23:08:06 -0300

    Implementa o cadastro no main e adicionar novo menu para o usuario logado

--- RedeSocial/src/redesocial/Aplicacao.java
+++ RedeSocial/src/redesocial/Aplicacao.java
@@ -6,6 +6,10 @@ import redesocial.controllers.GerenciamentoConta;
 import redesocial.models.Conta;
 
 public class Aplicacao {
+    public int menuLogado(Conta conta) {
+        System.out.printf("Bem vindo %s\n", conta.getNomeUsuario());
+    }
+
     public static void main(String[] args) {
         GerenciamentoConta gerenciamentoConta = new GerenciamentoConta();
         boolean sair = false;
@@ -21,11 +25,21 @@ public class Aplicacao {
 
             switch(in.nextInt()) {
                 case 1:
+                    System.out.println("Digite o login:");
+                    String login = in.next();
+                    System.out.println("Digite a senha:");
+                    String senha = in.next();
+                    System.out.println("Digite o nome do usuario:");
+                    String usuario = in.next();
+                    gerenciamentoConta.cadastrar(login, senha, usuario);
                     break;
                 case 2:
+                    System.out.println("Digite o login:");
                     String login = in.next();
+                    System.out.println("Digite a senha:");
                     String senha = in.next();
                     conta = gerenciamentoConta.logar(login, senha);
+                    sair = menuLogado(conta);
                     break;
                 case 3:
                     sair = true;
