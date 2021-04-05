e94ba6268428dcac86eabb245a5806280ed31b8f
marcosfandrade <marcos.fellipe2211@gmail.com>
2021-04-04 22:09:39 -0300

    adicao de recado

--- RedeSocial/src/redesocial/models/Perfil.java
+++ RedeSocial/src/redesocial/models/Perfil.java
@@ -1,18 +1,28 @@
 
 package redesocial.models;
 
+import java.util.ArrayList;
+import redesocial.models.Recado;
+
 public class Perfil implements Comparable <Perfil> {
     //atributos
     private String nomePerfil; 
     private String cidade;
     private String fone;
     private int idade;
+    private ArrayList<Recado> recados;
+    private ArrayList<String> convites;
+    private ArrayList<String> amigos;
+
     //CONSTRUTOR
     public Perfil (String nomePerfil, String cidade, String fone, int idade) {
         this.nomePerfil = nomePerfil;
         this.cidade = cidade;
         this.idade = idade;
         this.fone = fone;
+        this.recados = new ArrayList<Recado>();
+        this.convites = new ArrayList<String>();
+        this.amigos = new ArrayList<String>();
     }
     //COnstrutor 2
     public Perfil (String nomePerfil) {
@@ -52,5 +62,38 @@ public class Perfil implements Comparable <Perfil> {
     @Override
     public int compareTo (Perfil p) {
         return this.getNome().compareTo(p.getNome());
+    }
+
+    public void enviarRecado(String msg, String autor){
+        Recado recado = new Recado(msg, autor);
+        recados.add(recado);
+    }
+
+    public ArrayList<Recado> getRecados() {
+        return this.recados;
+    }
+
+    public void enviarConvite(String solicitante) {
+        this.convites.add(solicitante);
+    }
+
+    public void aceitarConvite(Perfil perfil) {
+        boolean achouConvite = false;
+        int index = 0;
+        String novoAmigo = perfil.getNome();
+
+        for (int i=0; i < this.convites.size(); i++){
+            if (this.convites.get(i) == novoAmigo) {
+                achouConvite = true;
+                index = i;
+                break;
+            }
+        }
+
+        if (achouConvite) {
+            this.convites.remove(index);
+            this.amigos.add(novoAmigo);
+            perfil.amigos.add(this.nomePerfil);
+        }
     }
 }
--- RedeSocial/src/redesocial/models/Recado.java
+++ RedeSocial/src/redesocial/models/Recado.java
@@ -1,6 +1,24 @@
-
 package redesocial.models;
 
 public class Recado {
-    
+
+    private String mensagem;
+    private String autor;
+
+    public Recado(String mensagem, String autor) {
+        this.mensagem = mensagem;
+        this.autor = autor;
+    }
+
+    public String getMensagem() {
+        return this.mensagem;
+    }
+
+    public String getAutor() {
+        return this.autor;
+    }
+
+    public String toString() {
+        return "Mensagem: " + this.mensagem + "Enviada por: " + this.autor;
+    }
 }
